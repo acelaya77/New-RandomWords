6d9b9fb5d8f78027a8f83faead0ac8c8738f91d1
Anthony J. Celaya <anthony.celaya@scccd.edu>
2019-07-12 17:52:50 -0700

    removed old method for importing Exchange on-prem modules & added prefix

--- SCCCDAccounts/Test-EmailAddressAvailable.ps1
+++ SCCCDAccounts/Test-EmailAddressAvailable.ps1
@@ -7,15 +7,16 @@ AUTHOR      : Anthony J. Celaya
 DESCRIPTION : Test for email address availability.
 MODULES     : 
 GLOBAL VARS : 
-LAST RAN    : 12-13-2018 
-UPDATED     : 12-13-2018
-VERSION     : 1.0
+LAST RAN    : 07-09-2019 
+UPDATED     : 07-09-2019
+VERSION     : 1.1
 
 
 
 Ver EntryDate  Editor Description    
 --- ---------  ------ -----------    
 1.0 12-13-2018 ac007  INITIAL RELEASE
+1.1 07-09-2019 ac007  Discontinue use of Connect-Exchange
 
 #>
 #endregion
@@ -27,23 +28,24 @@ Function Test-EmailAddressAvailable{
     Param(
         [Parameter(Mandatory=$true)][string]$EmailAddress
     )
-    Connect-Exchange
-    #$sqlResults = Get-SQLWebAdvisorID -EmployeeIDs $EmployeeID
-    <#
-    $name = $("{0} {1}" -f $sqlResults.Givenname,$sqlResults.Surname)
-    $smtp = $("{0}.{1}" -f $sqlResults.Givenname.Tolower().Replace(' ','-'),$sqlResults.Surname.ToLower().Replace(' ','-'))
-    $givenname = $("{0}" -f $sqlResults.Givenname,$sqlResults.Surname)
-    $surname = $("{1}" -f $sqlResults.Givenname,$sqlResults.Surname)
-    #>
-    <#
-    Try{
-        #$mailBox = Get-Mailbox -Filter {(EmailAddresses -like "smtp:$($smtp)*")} -ErrorAction Stop -DomainController SDODC1-08e
-    }
-    Catch{
-        $true
-    }
-    #>
-    $mailBox = Get-Mailbox "$($EmailAddress)" -DomainController $DomainController -ErrorAction SilentlyContinue
+
+    
+    if(!(Get-PSSession).ComputerName -eq 'sdoex01.scccd.net'){
+        $Url_OnPrem = $("http://{0}.scccd.net/powershell" -f "SDOEX01")
+
+        $splat_onprem = @{
+            ConfigurationName = 'Microsoft.Exchange'
+            ConnectionUri = $Url_OnPrem
+            Authentication = 'Kerberos'
+            AllowRedirection = $true
+            Name = "SCCCD.Exchange.OnPrem"
+        }
+        $session_onPrem = New-PSSession @splat_onprem
+        Import-PSSession -Session $session_onPrem -CommandName * -FormatTypeName * -Prefix "onPrem"
+        }#end if
+
+
+    $mailBox = Get-onPremMailbox "$($EmailAddress)" -DomainController $DomainController -ErrorAction SilentlyContinue
     if($null -ne $mailBox.PrimarySMTPAddress){
         [bool]$Available = $false
     }
