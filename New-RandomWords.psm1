851439971c42517186ac7822edfd6adc8dfc15bc
Anthony J. Celaya <ac007@Scccd.edu>
2020-05-15 17:21:07 -0700

    new list for passwd generator with CURL download

--- New-RandomWords.psm1
+++ New-RandomWords.psm1
@@ -4,11 +4,31 @@ Function New-RandomWords {
     Param(
         [int]$WordCount = 3,
         [int]$MaxCharacterLength = 6,
-        [Switch]$useWords
+        [Switch]$useWords,
+        [Switch]$Download
     )#end Param()
 
     #Let's remove any variables
     @("words","rndWords","strPassword","password","myReturnObject") | Get-Variable -Scope Script -ErrorAction SilentlyContinue -ErrorVariable getVarErrors | Remove-Variable -ErrorAction SilentlyContinue -ErrorVariable removeVarErrors
+
+    #use this to generate the list from a free API
+    if($PSBoundParameters.ContainsKey("Download")){
+        $path = "$env:USERPROFILE\Repositories\my_modules\SCCCDModules\New-RandomWords\words.txt"
+        $path = (Join-Path "$PSScriptRoot" "words.txt")
+        Invoke-WebRequest "https://random-word-api.herokuapp.com/all?swear=0" -OutFile $path
+
+        $counter = 0
+        $words = @()
+        $(Get-Content $path).replace("[", "").replace("]", "").split(",").where( { $_.Length -lt 8 -and $_.length -gt 2 }) | ForEach-Object { $words += [PSCustomObject]@{
+                Index = $("{0:00000}" -f $counter++)
+                Word  = $($_).replace('"',"")
+            } }
+        <#
+        $words[0..10]
+        $words[-1]
+        #>
+        $words | Export-Csv -Delimiter "," -Nti (Join-Path (split-Path $path -Parent) (Split-Path $path -Leaf).replace("txt","csv"))
+    }
 
     #$words = Get-Content (Join-Path $PSScriptRoot "words.txt")
     if($PSBoundParameters.ContainsKey('useWords')){
@@ -18,16 +38,15 @@ Function New-RandomWords {
         #$words | Get-Member
 
     }else{
-        $words = $(Import-Csv -Delimiter "`t" (Join-Path $PSScriptRoot "eff_large_wordlist.csv").where({$_ -notmatch "badass"}))
-        <#
-        $words = Import-Csv -Delimiter "`t" (Join-Path "E:\My_Docs\repos\Modules\SCCCDModules\New-RandomWords" "eff_large_wordlist.csv")
-        #>
-        #$words = $words.where({$PSItem -notmatch "badass"})
+        #$words = $(Import-Csv -Delimiter "`t" (Join-Path $PSScriptRoot "eff_large_wordlist.csv").where({$_ -notmatch "ass|douche|(wo)?man"}))
+        $words = $(Import-Csv -Delimiter "," (Join-Path $PSScriptRoot "words.csv"))
     }
+
     Write-Verbose "Word count: $($words.count)"
     $script:rndWords = $Null
     while([string]::IsNullOrEmpty($script:rndWords)){
-        $script:rndWords = $(Get-Random -InputObject $($words.word.ToLower().Where({$PSItem.Length -le $MaxCharacterLength})) -Count $WordCount)
+        #$script:rndWords = $(Get-Random -InputObject $($words.word.ToLower().Where({$PSItem.Length -le $MaxCharacterLength})) -Count $WordCount)
+        $script:rndWords = $(Get-Random -InputObject $($words.word.ToLower()) -Count $WordCount)
     }
     $strPassword = $("{0} {1}{2}" -f $((get-Culture).TextInfo.ToTitleCase($script:rndWords[0])),$($script:rndWords[1..$($script:rndWords.Count)] -join ' '),$(@('.','?','!') | Get-Random))
     $password = $(ConvertTo-SecureString -Force -AsPlainText $strPassword)
